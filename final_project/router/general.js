e8c76cb4adffa1f8093741b7e7fcf561b5e96210
Chris Deugau <83315901+chrisdeugau@users.noreply.github.com>
2025-03-08 18:59:16 -0800

    filter by author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -23,8 +23,11 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.author;
+
+    const output = Object.values(books).filter(book => book.author === author);
+
+    res.send(output);
 });
 
 // Get all books based on title
