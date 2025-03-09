ad3cf9087e7705ec636887861f8a0dcf031a1fa7
Chris Deugau <83315901+chrisdeugau@users.noreply.github.com>
2025-03-09 15:06:19 -0700

    add promise code

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -91,4 +91,84 @@ public_users.get('/review/:isbn',function (req, res) {
     return res.status(404).json({message: "Book doesn't exist"});
 });
 
+
+// Get the book list by Promises
+public_users.get('/' , async (req,res) => {
+    try {
+        const library = await new Promise((resolve,reject)=>{
+            if (books) {
+                resolve(books);
+            } else {
+                reject(new Error('Error fetching books'));
+            }
+        });
+
+        return res.status(200).json(library);
+    } catch (error) {
+        res.status(404).json({message:'Error fetching books', error: error.message});
+    }
+});
+
+// Get book details by promises
+ public_users.get('/isbn/:isbn', async (req,res) => {
+    const isbn = req.params.isbn;
+ 
+    new Promise((resolve,reject) => {
+        const book = books[isbn];
+        if (book) {
+            resolve(book);
+        } else {
+            reject("Book not found");
+        }
+    })
+    .then((book) => {
+        res.status(200).json(book)
+    })
+    .catch((error)=>{
+        res.status(404).json({message:"Book not found"})
+    })
+ 
+ });
+
+// Get book details with Promises
+public_users.get('/author/:author', function(req,res) {
+    const author = req.params.author;
+ 
+    new Promise((resolve,reject) => {
+        const foundBooks = Object.values(books).filter(book => book.author === author);
+        if (foundBooks.length > 0) {
+            resolve(foundBooks)
+        } else {
+            reject("0 books found")
+        }
+    })
+    .then((foundBooks)=>{
+        res.status(200).json(foundBooks);
+    })
+    .catch((error)=>{
+        res.status(404).json({message:"no books found"});
+    })
+});
+
+// Get books with Promises
+public_users.get('/title/:title', function(req,res){
+    const title = req.params.title;
+ 
+    new Promise((resolve,reject) => {
+        const booksFound = Object.values(books).filter(book => book.title === title);
+        if (booksFound.length > 0) {
+            resolve(booksFound);
+        } else {
+            reject("0 books found");
+        }
+    })
+    .then((booksFound)=>{
+        return res.status(200).json(booksFound);
+    })
+    .catch((error)=>{
+        return res.status(404).json({message:"No books found"});
+    })
+});
+
+
 module.exports.general = public_users;
